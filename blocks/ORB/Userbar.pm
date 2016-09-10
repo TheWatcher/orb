88475b0f05218464a5fad942706877dd4b005228
Chris <chris@starforge.co.uk>
2016-09-10 15:47:35 +0100

    Initial commit based on Aviary core

---
+++ blocks/ORB/Userbar.pm
@@ -0,0 +1,122 @@
+## @file
+# This file contains the implementation of the ORB user toolbar.
+#
+# @author  Chris Page &lt;chris@starforge.co.uk&gt;
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## @class ORB::Userbar
+# The Userbar class encapsulates the code required to generate and
+# manage the user toolbar.
+package ORB::Userbar;
+
+use strict;
+use parent qw(ORB);
+use v5.12;
+
+
+# ==============================================================================
+#  Bar generation
+
+## @method $ block_display($title, $current, $doclink)
+# Generate a user toolbar, populating it as needed to reflect the user's options
+# at the current time.
+#
+# @param title   A string to show as the page title.
+# @param current The current page name.
+# @param doclink The name of a document link to include in the userbar. If not
+#                supplied, no link is shown.
+# @return A string containing the user toolbar html on success, undef on error.
+sub block_display {
+    my $self    = shift;
+    my $title   = shift;
+    my $current = shift;
+    my $doclink = shift;
+
+    $self -> clear_error();
+
+    my $loginurl = $self -> build_url(block => "login",
+                                      fullurl  => 1,
+                                      pathinfo => [],
+                                      params   => {},
+                                      forcessl => 1);
+
+    my $fronturl = $self -> build_url(block    => $self -> {"settings"} -> {"config"} -> {"default_block"},
+                                      fullurl  => 1,
+                                      pathinfo => [],
+                                      params   => {});
+
+    # Initialise fragments to sane "logged out" defaults.
+    my ($import, $userprofile, $docs) =
+        ($self -> {"template"} -> load_template("userbar/import_disabled.tem"),
+         $self -> {"template"} -> load_template("userbar/profile_loggedout_http".($ENV{"HTTPS"} eq "on" ? "s" : "").".tem", {"***url-login***" => $loginurl}),
+         $self -> {"template"} -> load_template("userbar/doclink_disabled.tem"),
+        );
+
+    # Is documentation available?
+    my $url = $self -> get_documentation_url($doclink);
+    $docs = $self -> {"template"} -> load_template("userbar/doclink_enabled.tem", {"***url-doclink***" => $url})
+        if($url);
+
+    # Is the user logged in?
+    if(!$self -> {"session"} -> anonymous_session()) {
+        my $user = $self -> {"session"} -> get_user_byid()
+            or return $self -> self_error("Unable to obtain user data for logged in user. This should not happen!");
+
+        $import  = $self -> {"template"} -> load_template("userbar/import_enabled.tem"  , {"***url-import***" => $self -> build_url(block => "import", pathinfo => [])})
+            if($self -> check_permission("import") && $current ne "import");
+
+        # User is logged in, so actually reflect their current options and state
+        $userprofile = $self -> {"template"} -> load_template("userbar/profile_loggedin.tem", {"***realname***"    => $user -> {"fullname"},
+                                                                                               "***username***"    => $user -> {"username"},
+                                                                                               "***gravhash***"    => $user -> {"gravatar_hash"},
+                                                                                               "***url-logout***"  => $self -> build_url(block => "login"  , pathinfo => ["logout"])});
+    } # if(!$self -> {"session"} -> anonymous_session())
+
+    return $self -> {"template"} -> load_template("userbar/userbar.tem", {"***pagename***"  => $title,
+                                                                          "***front_url***" => $fronturl,
+                                                                          "***import***"    => $import,
+                                                                          "***doclink***"   => $docs,
+                                                                          "***profile***"   => $userprofile});
+}
+
+
+## @method $ page_display()
+# Produce the string containing this block's full page content. This is primarily provided for
+# API operations that allow the user to change their profile and settings.
+#
+# @return The string containing this block's page content.
+sub page_display {
+    my $self = shift;
+    my ($content, $extrahead, $title);
+
+    if(!$self -> {"session"} -> anonymous_session()) {
+        my $user = $self -> {"session"} -> get_user_byid()
+            or return '';
+
+        my $apiop = $self -> is_api_operation();
+        if(defined($apiop)) {
+            given($apiop) {
+                default {
+                    return $self -> api_html_response($self -> api_errorhash('bad_op',
+                                                                             $self -> {"template"} -> replace_langvar("API_BAD_OP")))
+                }
+            }
+        }
+    }
+
+    return "<p class=\"error\">".$self -> {"template"} -> replace_langvar("BLOCK_PAGE_DISPLAY")."</p>";
+}
+
+1;
---
+++ modules/ORB/System/Entity.pm
@@ -0,0 +1,376 @@
+## @file
+# This file contains the implementation of the Entity base class.
+#
+# @author  Chris Page &lt;chris@starforge.co.uk&gt;
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## @class Entity
+# This is a base class for entities in the system, providing common
+# features for all of the entities. Entities are any simple named
+# object in the system, typically things like ingredients, prep
+# memthods, recipe types and states.
+#
+# Tables for entities must have a minimum of the following fields:
+#
+# - `id`: unsigned int or larger, auto incremement
+# - `name`: varchar ot text, utf8_unicode_ci charset recommended
+# - `refcount`: unsigned int recommended
+#
+package ORB::System::Entity;
+
+use strict;
+use parent qw(Webperl::SystemModule);
+use v5.14;
+
+use Webperl::Utils qw(hash_or_hashref);
+
+
+# ============================================================================
+#  Constructor
+
+## @cmethod $ new(%args)
+# Create a new Entity object to manage entity creation and management.
+# The minimum values you need to provide are:
+#
+# - `dbh`          - The database handle to use for queries.
+# - `settings`     - The system settings object
+# - `logger`       - The system logger object.
+# - `entity_table` - The name of the table the entities are stored in.
+#
+# @param args A hash of key value pairs to initialise the object with.
+# @return A new Entity object, or undef if a problem occured.
+sub new {
+    my $invocant = shift;
+    my $class    = ref($invocant) || $invocant;
+    my $self     = $class -> SUPER::new(@_);
+
+    return SystemModule::set_error("No entity table specified when attempting to create object")
+        if(!$self -> {"entity_table"});
+
+    return $self
+}
+
+
+# ============================================================================
+#  Entity creation and deletion
+
+## @method $ create($name)
+# Attempt to create a new named entity in the entoty table. Generally you
+# should not call this directly, as it will create a new entity in the table
+# even if an entity already exists with the same name: you will generally
+# want to call get_id() instead, as that will determine whether that the
+# entity already exists before calling this if it does not.
+#
+# @param name The name of the entity to add.
+# @return The new entity ID on success, undef on error.
+sub create {
+    my $self  = shift;
+    my $name  = shift;
+
+    $self -> clear_error();
+
+    my $newh = $self -> {"dbh"} -> prepare("INSERT INTO `".$self -> {"settings"} -> {"database"} -> {$self -> {"entity_table"}}."`
+                                            (`name`)
+                                            VALUES(?)");
+    my $rows = $newh -> execute($name);
+    return $self -> self_error("Unable to perform ".$self -> {"entity_table"}." insert: ". $self -> {"dbh"} -> errstr) if(!$rows);
+    return $self -> self_error($self -> {"entity_table"}." insert failed, no rows inserted") if($rows eq "0E0");
+
+    # FIXME: This ties to MySQL, but is more reliable that last_insert_id in general.
+    #        Try to find a decent solution for this mess...
+    # NOTE: the DBD::mysql documentation doesn't actually provide any useful information
+    #       about what this will contain if the insert fails. In fact, DBD::mysql calls
+    #       libmysql's mysql_insert_id(), which returns 0 on error (last insert failed).
+    #       There, why couldn't they bloody /say/ that?!
+    my $id = $self -> {"dbh"} -> {"mysql_insertid"};
+    return $self -> self_error("Unable to obtain id for entity '$name'") if(!$id);
+
+    return $id;
+}
+
+
+## @method $ destroy(%args)
+# Attempt to remove the specified entity, and any assignments of it, from the system.
+# Supported arguments are:
+#
+# - `id`:        The ID of the entity to remove. If not specified, a name must be given.
+# - `name`:      The name of the entity to remove. If ID is specified, this is ignored.
+# - `relations`: A hash containing `name` and `field` fields specifying the table
+#                to remove any relations from. This may be specified as a
+#                reference to an array of hashes.
+#
+# @param args A hash, or reference to a hash, or argument.
+# @return true on success, undef on error
+sub destroy {
+    my $self = shift;
+    my $args = hash_or_hashref(@_);
+
+    $self -> clear_error();
+
+    return $self -> self_error("No id or name specified in call to destroy")
+        unless($args -> {"id"} || $args -> {"name"});
+
+    # Convert name to ID if needed
+    $args -> {"id"} = $self -> _fetch_id($args -> {"name"})
+        unless($args -> {"id"});
+
+    # fall over if the relations argument is specified, but it's not a hash or arrayref
+    return $self -> self_error("destroy invoked with invalid relations data")
+        if($args -> {"relations"} &&
+           ref($args -> {"relations"} ne "HASH" && ref($args -> {"relations"}) ne "ARRAY"));
+
+    # Force arrayref of hashes for simplicity
+    $args -> {"relations"} = [ $args -> {"relations"} ]
+        if($args -> {"relations"} && ref($args -> {"relations"} eq "HASH"));
+
+    # Process and remove each relation... or possibly none if there are none.
+    foreach my $relation (@{$args -> {"relations"}}) {
+        return $self -> self_error("Relation hash data invalid")
+            unless($relation -> {"table"} && $relation -> {"field"});
+
+        $self -> remove_relation($args -> {"id"}, $relation -> {"table"}, $relation -> {"field"})
+            or return undef;
+    }
+
+    # Check that the entity is safe to delete...
+    my $refcount = $self -> _fetch_refcount($id);
+
+    return $self -> self_error("Attempt to delete non-existent entity $id from ".$self -> {"entity_table"})
+        unless(defined($refcount));
+
+    return $self -> self_error("Attempt to delete entity $id in ".$self -> {"entity_table"}." while still in use ($refcount references)")
+        if($refcount);
+
+    # And now delete the entity itself
+    my $nukeh = $self -> {"dbh"} -> prepare("DELETE FROM `".$self -> {"settings"} -> {"database"} -> {$self -> {"entity_table"}}."`
+                                             WHERE `id` = ?");
+    $nukeh -> execute($args -> {"id"})
+        or return $self -> self_error("Unable to perform entity $id removal from ".$self -> {"entity_table"}.": ". $self -> {"dbh"} -> errstr);
+
+    return 1;
+}
+
+
+# ============================================================================
+#  Lookup
+
+## @method $ get_id($name)
+# Obtain the ID associated with the specified entity. If the entity
+# does not yet exist in the entity's table, this will create it and
+# return the id the new entity was allocated.
+#
+# @param name The name of the entity to obtain the ID for
+# @return The ID of the entity on success, undef on error.
+sub get_id {
+    my $self = shift;
+    my $name = shift;
+
+    my $id = $self -> _fetch_id($name);
+    return $id if(!defined($id) || $id); # return undefs, or non-zero ids.
+
+    # Get here, and $id is zero - no entity exists with the specified name, so
+    # a new entity is needed.
+    return $self -> create($name);
+}
+
+
+# ============================================================================
+#  Relation handling
+
+## @method $ remove_relation($id, $table, $field, $retain_unused)
+# Remove any relations to the specified entity from the table provided.
+# This will decrease the refcount for the entity.
+#
+# @param id    The Id of the entity to remove the relation for.
+# @param table The name of the table containing the relation to remove.
+# @param field The field containing the entity ID in the relation table.
+# @param retain_unused If true, do not delete the entity even if its refcount will
+#                      be zero after calling this. Defaults to true.
+# @return true on success, false on error.
+sub remove_relation {
+    my $self  = shift;
+    my $id    = shift;
+    my $table = shift;
+    my $field = shift;
+    my $retain_unused = shift // 1;
+
+    $self -> clear_error();
+
+    my $removeh = $self -> {"dbh"} -> prepare("DELETE FROM `$table`
+                                               WHERE `$field` = ?");
+    my $rows = $removeh -> execute($id);
+    return $self -> self_error("Unable to remove entity relations to $id from $table") if(!$rows);
+    return 1 if($rows eq "0E0"); # Zero row removal is possible and not an error
+
+    my $result = $self -> _update_refcount($id, subtract => $rows);
+    return undef if(!defined($result));
+
+    # Nuke the entity if there's no reason to keep it around.
+    return $self -> destroy($id)
+        unless($retain_unused || $result);
+
+    return defined($result);
+}
+
+# ============================================================================
+#  Reference counting
+
+## @method $ increase_refcount($id)
+# Increase the refcount for the entity with the specified id.
+#
+# @param id The ID of the entity to increase the refcount for.
+# @return true on success (actually, the reference count), false on error.
+sub increase_refcount {
+    my $self = shift;
+    my $id   = shift;
+
+    return $self -> _update_refcount($id, add => 1);
+}
+
+
+## @method $ decrease_refcount($id, $retain_unused)
+# Reduce the refcount for the entity with the specified id. If the refcount
+# becomes zero, and $retain_unused is not true, the entity is removed from
+# the system.
+#
+# @param id            The ID of the entity to decrease the refcount for.
+# @param retain_unused If true, do not delete the entity even if its refcount will
+#                      be zero after calling this. Defaults to true.
+# @return true on success, false on error.
+sub decrease_refcount {
+    my $self          = shift;
+    my $id            = shift;
+    my $retain_unused = shift // 1;
+
+    # Change the refcount, bomb if the change failed.
+    my $result = $self -> _update_refcount($id, subtract => 1);
+    return undef if(!defined($result));
+
+    # Nuke the entity if there's no reason to keep it around.
+    return $self -> destroy($id)
+        unless($retain_unused || $result);
+
+    return defined($result);
+}
+
+
+# ============================================================================
+#  ID lookup
+
+## @method protected $ _fetch_id($name)
+# Given an entity name, attempt to find a record for that name. This will locate the
+# first defined entity whose name matches the provided name. Note that if there are
+# duplicate entities in the system, this will never find duplicates - it is guaranteed to
+# find the entity with the lowest ID whose name matches the provided value, or nothing.
+#
+# @param name The name of the entity to find.
+# @return The ID of the entity with the specified name on success, 0 if it does not exist,
+#         undef on error occurred.
+sub _fetch_id {
+    my $self  = shift;
+    my $name  = shift;
+
+    $self -> clear_error();
+
+    # Simple lookup. If the name field is set up as a _ci field, this will be case insensitive
+    my $entityh = $self -> {"dbh"} -> prepare("SELECT `id`
+                                               FROM `".$self -> {"settings"} -> {"database"} -> {$self -> {"entity_table"}}."`
+                                               WHERE `name` LIKE ?
+                                               ORDER BY `id`
+                                               LIMIT 1"); # Limit to avoid pulling multiple rows if there are duplicates.
+    $entityh -> execute($name)
+        or return $self -> self_error("Unable to perform entity lookup: ".$self -> {"dbh"} -> errstr);
+
+    # Return the ID if we have located it
+    my $entityrow = $entityh-> fetchrow_arrayref();
+    return $entityrow ? $entityrow -> [0] : 0;
+}
+
+
+# ============================================================================
+#  Reference handling - generally intended only for subclasses to use
+
+## @method protected $ _fetch_refcount($id)
+# Obtain the reference count for the specified entity. This will attempt to
+# fetch the reference count for the entity, if it fails - because the entity
+# does not exist, or the database has shat itself - it will return undef.
+#
+# @param id The entity to fetch the reference count for.
+# @return The number of references to the entity (which may be zero), or undef
+#         on error.
+sub _fetch_refcount {
+    my $self = shift;
+    my $id   = shift;
+
+    $self -> clear_error();
+
+    # Simple lookup, nothing spectacular to see here...
+    my $refh = $self -> {"dbh"} -> prepare("SELECT `refcount`
+                                            FROM `".$self -> {"settings"} -> {"database"} -> {$self -> {"entity_table"}}."`
+                                            WHERE `id` = ?");
+    $refh -> execute($id)
+        or return $self -> self_error("Unable to perform entity refcount lookup: ". $self -> {"dbh"} -> errstr);
+
+    my $refcount = $refh -> fetchrow_arrayref();
+    return $refcount ? $refcount -> [0] : $self -> self_error("Unable to locate entity ".$self -> {"entity_table"}."[$id]: does not exist");
+}
+
+
+## @method protected $ _update_refcount($id, %operation)
+# Update the refcount for the specified entity. This will increment, decrement,
+# or set the value stored in the specified entity's reference counter. This is the
+# actual implementation underlying increase_refcount() and decrease_refcount().
+#
+# @param id        The ID of the entity to update the refcount for.
+# @param operation A hash containing one of 'add', 'subtract', or 'set' with
+#                  values that indicate how much to change the refcount by.
+# @return The new value of the reference count on success (which may be zero), undef on error.
+sub _update_refcount {
+    my $self      = shift;
+    my $id        = shift;
+    my %operation = @_;
+
+    $self -> clear_error();
+
+    my $refcount = $self -> _fetch_refcount($id);
+    return undef if(!defined($refcount));
+
+    # Calculate the new refcount
+    if(defined($operation{"add"})) {
+        $refcount += $operation{"add"};
+    } elsif(defined($operation{"subtract"})) {
+        $refcount -= $operation{"subtract"};
+    } elsif(defined($operation{"set"})) {
+        $refcount = $operation{"set"};
+    } else {
+        return $self -> self_error("No valid operation specified in call to _update_refcount() for ".$self -> {"entity_table"}."[$id]");
+    }
+
+    # Is the new refcount sane?
+    return $self -> self_error("New refount of $refcount for entity ".$self -> {"entity_table"}."[$id]: is invalid")
+        if($refcount < 0 || ($self -> {"max_refcount"} && $refcount > $self -> {"max_refcount"}));
+
+    # Update is safe, do the operation.
+    my $atth = $self -> {"dbh"} -> prepare("UPDATE `".$self -> {"settings"} -> {"database"} -> {$self -> {"entity_table"}}."`
+                                            SET `refcount` = ?
+                                            WHERE `id` = ?");
+    my $result = $atth -> execute($refcount, $id);
+    return $self -> self_error("Unable to update entity refcount: ".$self -> {"dbh"} -> errstr) if(!$result);
+    return $self -> self_error("Entity refcount update failed: no rows updated. This should not happen!") if($result eq "0E0");
+
+    return $refcount;
+}
+
+1;
